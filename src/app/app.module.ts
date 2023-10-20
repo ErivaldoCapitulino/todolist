8ca0ce37d3e2a34a913f5bc78c9506ba12803f60
ErivaldoCapitulino <vadositi@gmail.com>
2023-10-20 00:43:17 -0300

    Retirando o arquivo readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { CurrencyFormatPipe } from './pipes/currency-format.pipe';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateGreaterThanTodayDirective } from './directives/date-greater-than-today.directive';
+import { TaskFiltroComponent } from './components/task-filtro/task-filtro.component';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    CurrencyFormatPipe,
+    TaskDetailComponent,
+    DateGreaterThanTodayDirective,
+    TaskFiltroComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
